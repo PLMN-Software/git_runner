72be39e334e17a78d939884bd44ea295cde39acf
Frank Pollmann <frank.pollmann@outlook.de>
2025-07-12 14:20:35 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -e
 
-echo "🛠️  Root-Setup: Passe Docker-Gruppe und Rechte an…"
-
 DOCKER_SOCK="/var/run/docker.sock"
 if [ -S "$DOCKER_SOCK" ]; then
   DOCKER_GID=$(stat -c '%g' $DOCKER_SOCK)
-  if ! getent group docker | grep -q ":$DOCKER_GID:"; then
-    groupadd -for -g "$DOCKER_GID" docker
+  if getent group docker; then
+    # Ändere GID der bestehenden docker-Gruppe auf Host-GID!
+    groupmod -g "$DOCKER_GID" docker
+  else
+    groupadd -g "$DOCKER_GID" docker
   fi
   usermod -aG docker runneruser
 fi
 
+# Rest wie gehabt...
 echo "🔄 Wechsle jetzt zu runneruser…"
-exec gosu runneruser /entrypoint-user.sh
-
-
+exec gosu runneruser /runner/entrypoint-user.sh
