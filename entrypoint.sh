68a1b8e738ed7d4f24d3bbb7c2058ea567da665d
Frank Pollmann <frank.pollmann@outlook.de>
2025-07-12 02:27:47 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,9 @@ set -e
 
 echo "▶️ Starte GitHub Runner Setup..."
 
-./config.sh --url "$GITHUB_URL" --token "$GITHUB_TOKEN" --name "$RUNNER_NAME" --unattended
+#./config.sh --url "$GITHUB_URL" --token "$GITHUB_TOKEN" --name "$RUNNER_NAME" --unattended
+./config.sh --url https://github.com/PLMN-Software --token AD4MM353JMNUB7MDFE2ZIC3IOG4HC --name "PLMN-dev-001" --unattended
+
 
 echo "✅ Runner registriert. Starte..."
 
